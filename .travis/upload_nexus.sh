5f15abf977d00b6867409768d189481799f446a7
Adam J. Weigold <adam@adamweigold.com>
2017-08-29 15:16:02 -0500

    #7 Enabling publishing of snapshot builds to Maven Central

--- .travis/upload_nexus.sh
+++ .travis/upload_nexus.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export PUBLISH=true
 ./gradlew uploadArchives -Psigning.password=$signingPassword -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
 ./gradlew closeAndReleaseRepository -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
