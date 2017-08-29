e2d61ed05fdcb83ac164f82570f2109501125780
Adam J. Weigold <adam@adamweigold.com>
2017-08-29 18:40:19 -0500

    #7 Separating release promotion command as it's not necessary for SNAPSHOT releases on Maven Central.

--- .travis/upload_nexus.sh
+++ .travis/upload_nexus.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 ./gradlew uploadArchives -Psigning.password=$signingPassword -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
-./gradlew closeAndReleaseRepository -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
