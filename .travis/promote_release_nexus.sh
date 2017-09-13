8bba8af4c6b418598a2d5e95f8d9db74c96e43b6
Adam J. Weigold <adam@adamweigold.com>
2017-09-13 10:43:16 -0500

    Preparing 1.0 release

--- .travis/promote_release_nexus.sh
+++ .travis/promote_release_nexus.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+if [ "${RELEASE}" = "true" ]; then
 ./gradlew closeAndReleaseRepository -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
+fi
--- .travis/upload_nexus.sh
+++ .travis/upload_nexus.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+if [ "${RELEASE}" = "true" ]; then
 ./gradlew uploadArchives -Psigning.password=$signingPassword -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
+fi
