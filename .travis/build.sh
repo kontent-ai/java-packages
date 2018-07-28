dbd7ce306a2b2e13c171e4fc5c2f78584daca485
Adam J. Weigold <adam@adamweigold.com>
2018-07-28 16:57:22 -0500

    Updating build script to limit sonarqube to `master`.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e # Exit with nonzero exit code if anything fails
+SOURCE_BRANCH="master"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     ./gradlew build
@@ -13,13 +14,15 @@ else
     -PnexusUsername=$sonatypeUsername \
     -PnexusPassword=$sonatypePassword
 
-    ./gradlew sonarqube \
-    -Penable.signing=true \
-    -Psigning.keyId=D1115C87 \
-    -Psigning.password=$signingPassword \
-    -Psigning.secretKeyRingFile="$TRAVIS_BUILD_DIR/signing.gpg" \
-    -PnexusUsername=$sonatypeUsername \
-    -PnexusPassword=$sonatypePassword
+    if [ "$TRAVIS_BRANCH" == "$SOURCE_BRANCH" ]; then
+        ./gradlew sonarqube \
+        -Penable.signing=true \
+        -Psigning.keyId=D1115C87 \
+        -Psigning.password=$signingPassword \
+        -Psigning.secretKeyRingFile="$TRAVIS_BUILD_DIR/signing.gpg" \
+        -PnexusUsername=$sonatypeUsername \
+        -PnexusPassword=$sonatypePassword
+    fi
 
     ./gradlew publish \
     -Penable.signing=true \
