1a8713572660b6955990cdc4943a3d32d7847a83
Adam J. Weigold <adam@adamweigold.com>
2018-07-28 18:14:27 -0500

    Updating build script.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,7 +3,7 @@
 set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     ./gradlew build
 else
     ./gradlew build \
@@ -14,15 +14,13 @@ else
     -PnexusUsername=$sonatypeUsername \
     -PnexusPassword=$sonatypePassword
 
-    if [ "$TRAVIS_BRANCH" == "$SOURCE_BRANCH" ]; then
-        ./gradlew sonarqube \
-        -Penable.signing=true \
-        -Psigning.keyId=D1115C87 \
-        -Psigning.password=$signingPassword \
-        -Psigning.secretKeyRingFile="$TRAVIS_BUILD_DIR/signing.gpg" \
-        -PnexusUsername=$sonatypeUsername \
-        -PnexusPassword=$sonatypePassword
-    fi
+    ./gradlew sonarqube \
+    -Penable.signing=true \
+    -Psigning.keyId=D1115C87 \
+    -Psigning.password=$signingPassword \
+    -Psigning.secretKeyRingFile="$TRAVIS_BUILD_DIR/signing.gpg" \
+    -PnexusUsername=$sonatypeUsername \
+    -PnexusPassword=$sonatypePassword
 
     ./gradlew publish \
     -Penable.signing=true \
