5259a3a227b638710b4cbe119779bf4358bf21d7
ondrejch <ondrejch@kentico.com>
2020-10-13 13:28:13 +0200

    add pararel load for listing

--- sample-app-android-kotlin/src/main/java/kentico/kontent/delivery/sample/dancinggoat/MainActivity.kt
+++ sample-app-android-kotlin/src/main/java/kentico/kontent/delivery/sample/dancinggoat/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.RequiresApi
+import android.support.annotation.WorkerThread
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import kentico.kontent.delivery.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.future.await
+import java.util.function.BiConsumer
 import java.util.function.Consumer
 
 class MainActivity : AppCompatActivity() {
@@ -19,14 +23,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         listView = findViewById<ListView>(R.id.articles_list_view);
-        val client = DeliveryClient(DeliveryOptions("975bf280-fd91-488c-994c-2f04416e5ee3"), null)
 
-        val params = DeliveryParameterBuilder.params().filterEquals("system.type", "article").build()
+        GlobalScope.launch(Dispatchers.Main) {
+            val articles = withContext(Dispatchers.IO) { loadArticles() }
+            displayArticles(articles);
+        }
+    }
 
-        val articles = client.getItems(params).toCompletableFuture().get().items
-
+    private fun displayArticles(articles: MutableList<ContentItem>) {
         val listItems = arrayOfNulls<String>(articles.size);
-
         for (i in 0 until articles.size) {
             val article = articles[i]
             listItems[i] = article.getString("title")
@@ -35,4 +40,15 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
         listView.adapter = adapter
     }
+
+    @WorkerThread
+    private suspend fun loadArticles(): MutableList<ContentItem> {
+        val client = DeliveryClient(DeliveryOptions("975bf280-fd91-488c-994c-2f04416e5ee3"), null)
+
+        val params = DeliveryParameterBuilder.params().filterEquals("system.type", "article").build()
+
+        val articles = client.getItems(params).await().items;
+        return articles;
+    }
+
 }
