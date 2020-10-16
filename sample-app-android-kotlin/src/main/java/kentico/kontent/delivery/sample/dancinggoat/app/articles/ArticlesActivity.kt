bda9f91ab4b8bc357d3c7971b9bfbb585fea4ad1
ondrejch <ondrejch@kentico.com>
2020-10-16 14:07:21 +0200

    finish kotlin articles loader

--- sample-app-android-kotlin/src/main/java/kentico/kontent/delivery/sample/dancinggoat/app/articles/ArticlesActivity.kt
+++ sample-app-android-kotlin/src/main/java/kentico/kontent/delivery/sample/dancinggoat/app/articles/ArticlesActivity.kt
@@ -1,26 +1,23 @@
-package kentico.kontent.delivery.sample.dancinggoat
+package kentico.kontent.delivery.sample.dancinggoat.app.articles
 
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.annotation.WorkerThread
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
 import kentico.kontent.delivery.*
+import kentico.kontent.delivery.sample.dancinggoat.R
+import kentico.kontent.delivery.sample.dancinggoat.models.Article
 import kotlinx.coroutines.*
 import kotlinx.coroutines.future.await
-import java.util.function.BiConsumer
-import java.util.function.Consumer
 
-class MainActivity : AppCompatActivity() {
+class ArticlesActivity : AppCompatActivity() {
     private lateinit var listView: ListView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.articles_activity)
 
         listView = findViewById<ListView>(R.id.articles_list_view);
 
@@ -30,25 +27,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun displayArticles(articles: MutableList<ContentItem>) {
-        val listItems = arrayOfNulls<String>(articles.size);
-        for (i in 0 until articles.size) {
-            val article = articles[i]
-            listItems[i] = article.getString("title")
-        }
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-        listView.adapter = adapter
+    private fun displayArticles(articles: MutableList<Article>) {
+        listView.adapter = ArticlesAdapter(this,  articles.toList())
     }
 
     @WorkerThread
-    private suspend fun loadArticles(): MutableList<ContentItem> {
+    private suspend fun loadArticles(): MutableList<Article> {
         val client = DeliveryClient(DeliveryOptions("975bf280-fd91-488c-994c-2f04416e5ee3"), null)
+        client.registerType(Article::class.java)
 
         val params = DeliveryParameterBuilder.params().filterEquals("system.type", "article").build()
-
-        val articles = client.getItems(params).await().items;
-        return articles;
+        return client.getItems(Article::class.java, params).await();
     }
 
 }
