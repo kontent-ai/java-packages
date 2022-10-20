820e88622f4b65e56e5e164c19e83c73000987a5
Ondřej Chrastina <chrastina.ondra@gmail.com>
2022-10-21 00:49:11 +0200

    add URL link resolution

--- sample-app-spring-boot/src/main/java/kontent/ai/delivery/sample/dancinggoat/springboot/KontentConfiguration.java
+++ sample-app-spring-boot/src/main/java/kontent/ai/delivery/sample/dancinggoat/springboot/KontentConfiguration.java
@@ -1,9 +1,11 @@
 package kontent.ai.delivery.sample.dancinggoat.springboot;
 
+import kontent.ai.delivery.ContentLinkUrlResolver;
 import kontent.ai.delivery.DeliveryClient;
 import kontent.ai.delivery.DeliveryOptions;
 import kontent.ai.delivery.Header;
 import kontent.ai.delivery.InlineContentItemsResolver;
+import kontent.ai.delivery.Link;
 import kontent.ai.delivery.sample.dancinggoat.models.Tweet;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.context.annotation.Bean;
@@ -27,10 +29,8 @@ public class KontentConfiguration {
                         .builder()
                         .projectId("975bf280-fd91-488c-994c-2f04416e5ee3")
                         .customHeaders(Arrays.asList(
-                                new Header(TRACKING_HEADER_NAME, TRACKING_HEADER_VALUE)
-                        ))
-                        .build()
-        );
+                                new Header(TRACKING_HEADER_NAME, TRACKING_HEADER_VALUE)))
+                        .build());
 
         client.scanClasspathForMappings("kontent.ai.delivery.sample.dancinggoat.models");
 
@@ -53,13 +53,27 @@ public class KontentConfiguration {
 
                 String options = String.format("&hide_thread=%b&hide_media=%b", hideThread, hideMedia);
                 String tweetLink = item.getTweetLink();
-                String url = String.format("https://publish.twitter.com/oembed?url=%s&theme=%s%s", tweetLink, theme, options);
+                String url = String.format("https://publish.twitter.com/oembed?url=%s&theme=%s%s", tweetLink, theme,
+                        options);
 
                 RestTemplate restTemplate = new RestTemplate();
                 String responseBody = restTemplate.getForObject(url, String.class);
                 Map<String, Object> object = JsonParserFactory.getJsonParser().parseMap(responseBody);
                 return (String) object.get("html");
             }
+        });
+
+        client.setContentLinkUrlResolver(new ContentLinkUrlResolver() {
+            @Override
+            public String resolveLinkUrl(Link link) {
+                switch (link.getType()) {
+                    case "article":
+                        return "/articles/" + link.getUrlSlug();
+                    default:
+                        return "";
+                }
+            }
+
         });
         return client;
     }
